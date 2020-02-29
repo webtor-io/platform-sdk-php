ac31c71236a8bc367805970a902fd9385bff0c17
Pavel Tatarskiy <pavel.tatarsky@gmail.com>
2020-02-29 18:28:35 +0300

    add token expiration

--- src/Webtor/SDK/Auth.php
+++ src/Webtor/SDK/Auth.php
@@ -9,15 +9,19 @@ class Auth
 
     public function __construct($params)
     {
-        $this->params = $params;
+        $defaults = [
+            'tokenExpire' => 5 * 60 * 60, // 5 hours
+        ];
+        $this->params = array_merge($defaults, $params);
     }
 
     public function getToken()
     {
-        $payload = null;
+        $payload = [];
         if (isset($this->params['claims']) && $this->params['claims']) {
             $payload = $this->params['claims'];
         }
+        $payload['exp'] = time() + $this->params['tokenExpire'];
         $token = JWT::encode($payload, $this->params['secret']);
         return $token;
     }
